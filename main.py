13a89cab6cd83f5e3b830f5bc295944847f6ba91
dkrivoshapova <d.krivoshapova@g.nsu.ru>
2020-11-16 10:30:06 +0700

    count

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
+# Функция полезности потребителя имеет вид U=A*X**q*Y**p(0<q,p<=1). Доход потребителя равен I.
+# Изначальные цены товаров Х,У заданы и равны соостветственно Рх,Ру. Известно также менение цены товара X.
+# Найти и вывести изначальный и конечный(после изменения цены) оптимальный набор потребителя.
+# По значениям эффекта дохода и эффекта замены определить вид товара Х.
+a = int(input('Введите значение коэффициента А '))
+q = float(input('Введите показатель степени Х '))
+p = float(input('Введите показатель степени Y '))
+i = int(input('Введите доход потребителя '))
+px = int(input('Введите цену товара Х '))
+py = int(input('Введите цену товара Y '))
+px2 = int(input('Введите цену товара Х после изменения цены '))
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if q < 0 or p < 0 or q > 1 or p > 1:
+    print('Программа не может решить такую задачу')
+# определение изначального оптимального набора
+x1 = i / (px * (1 + p / q))
+y1 = i / (py * (1 + q / p))
+print('Оптимальный набор потреителя до изменения цен', x1, y1)
+# определение  оптимального набора после изменения цен
+x3 = i / (px2 * (1 + p / q))
+y3 = i / (py * (1 + q / p))
+print('Оптимальный набор потреителя до изменения цен', x3, y3)
+# определение промежуточного набора
+i1 = x1 * px2 + y1 * py
+x3 = i1 / (px2 * (1 + p / q))
+y3 = i1 / (py * (1 + q / p))
